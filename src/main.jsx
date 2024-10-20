c2303b7bb375c9eb8c605e5ed915228ed65585f5
PC <mixail.ermin33@gmail.com>
2024-10-20 21:09:28 +0300

    login screen & sidebar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom'
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+// 3. extend the theme
+const myTheme = extendTheme({ config })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> 
+      <ChakraProvider theme={myTheme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+
   </StrictMode>,
 )
